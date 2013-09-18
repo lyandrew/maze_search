8a411cb83772fdecb208899b7834254b246618f3
hempywh <will.hempy@gmail.com>
2013-09-18 15:16:18 -0500

    added extra files

---
+++ main.cpp
@@ -0,0 +1,25 @@
+// CS 440 MP1
+// @author Will Hempy
+// @netid hempy2
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+
+	ifstream myReadFile;
+	myReadFile.open("text.txt");
+	char output[100];
+	if (myReadFile.is_open()) {
+	while (!myReadFile.eof()) {
+
+
+	myReadFile >> output;
+	cout<<output;
+
+
+	}
+}
+myReadFile.close();
