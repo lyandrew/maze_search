23382c413e257c16bce3372f79962ad644217bad
Andrew Ly <lyandrewf@gmail.com>
2013-09-20 19:58:34 -0500

    Reads file and convert to array

--- main.cpp
+++ main.cpp
@@ -4,22 +4,54 @@
 
 #include <iostream>
 #include <fstream>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
 
+//0 - wall
+//1 - blank
+//3 - dot
+//2 - packman
 using namespace std;
 
-int main(){
+int main(int argc, char **argv) {
+//	string bigMazeString = "bigMaze.lay";
+//	string openMazeString = "openMaze.lay";
+//	string mediumMazeString = "mediumMaze.lay";
+//	string smallMazeString = "smallMaze.lay";
 
-	ifstream myReadFile;
-	myReadFile.open("text.txt");
-	char output[100];
-	if (myReadFile.is_open()) {
-	while (!myReadFile.eof()) {
-
-
-	myReadFile >> output;
-	cout<<output;
-
-
+	ifstream myReadFile("bigMaze.lay");
+	string str;
+	int bigMaze[10000]; // arbitrary number of character in the maze file
+	int row = 0;
+	while( getline(myReadFile, str) ) {
+		int strSize = str.size();
+		if(strSize == 0 )
+			break;		
+		int j=0;		
+		for(j = 0; j < strSize; j++ ) {
+			if( str[j] == '%' )
+				bigMaze[str.size()*row + j] = 0;
+			else if(str[j] == ' ' )
+				bigMaze[str.size()*row + j] = 1;
+			else if( str[j] == '.' )
+				bigMaze[str.size()*row + j] = 3;
+			else //P
+				bigMaze[str.size()*row + j] = 2;
+		}
+		row++;
+		int numDots = 0;
+		if(str.find('.') != -1) numDots++;
 	}
+	
+	//test to see if the array works
+	// row is used her as the width
+	for( int i=0 ; i < row; i++ ) {
+		for( int j = 0; j < row; j++) {
+			cout << bigMaze[(row*i)+j];
+		}
+		printf("\n");
+	}
+
 }
-myReadFile.close();
+
