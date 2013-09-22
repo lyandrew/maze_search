eb30ab4729eb354777b8449f919fe6f7236587a0
Andrew Ly <lyandrewf@gmail.com>
2013-09-22 14:46:03 -0500

    Changed to 2d array

--- main.cpp
+++ main.cpp
@@ -12,46 +12,46 @@
 //1 - blank
 //3 - dot
 //2 - packman
+//bigMaze.lay;
+//openMaze.lay;
+//mediumMaze.lay;
+//smallMaze.lay;
+
 using namespace std;
 
 int main(int argc, char **argv) {
-//	string bigMazeString = "bigMaze.lay";
-//	string openMazeString = "openMaze.lay";
-//	string mediumMazeString = "mediumMaze.lay";
-//	string smallMazeString = "smallMaze.lay";
 
 	ifstream myReadFile("bigMaze.lay");
 	string str;
-	int bigMaze[10000]; // arbitrary number of character in the maze file
+	int bigMaze[100][100];
 	int row = 0;
-	while( getline(myReadFile, str) ) {
-		int strSize = str.size();
-		if(strSize == 0 )
-			break;		
-		int j=0;		
-		for(j = 0; j < strSize; j++ ) {
-			if( str[j] == '%' )
-				bigMaze[str.size()*row + j] = 0;
-			else if(str[j] == ' ' )
-				bigMaze[str.size()*row + j] = 1;
+	while (getline(myReadFile, str)) {
+	        int strSize = str.size();
+	        if (strSize == 0) break;
+	        int column = 0;
+	        for(int j =0; j < strSize; j++ ) {
+	                if (str[j] == '%')
+	                        bigMaze[row][j] = 0;
+	                else if(str[j] == ' ' )
+				bigMaze[row][j] = 1;
 			else if( str[j] == '.' )
-				bigMaze[str.size()*row + j] = 3;
-			else //P
-				bigMaze[str.size()*row + j] = 2;
-		}
-		row++;
-		int numDots = 0;
+				bigMaze[row][j] = 3;
+			else //Pacman
+				bigMaze[row][j] = 2;
+	        }       
+	        row++;
+	        int numDots = 0;
 		if(str.find('.') != -1) numDots++;
+		
+		cout << str << "  " << numDots << endl;
 	}
 	
-	//test to see if the array works
-	// row is used her as the width
-	for( int i=0 ; i < row; i++ ) {
-		for( int j = 0; j < row; j++) {
-			cout << bigMaze[(row*i)+j];
-		}
-		printf("\n");
+	// Testing if the array works
+	for ( int i = 0; i < row; i++) {
+	        for (int j = 0; j < row; j++) {
+	                cout << bigMaze[i][j];
+	        }
+	        printf("\n");
 	}
-
+	      
 }
-
